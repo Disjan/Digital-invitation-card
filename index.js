e3b7bd1f52fa5b8268e0773ec0857f70c06a6f3d
DH <80683691+Disjan@users.noreply.github.com.>
2025-04-24 12:11:50 +0200

    CSS Anpassungen

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 let birthdayChild = "/image/title.png";
+let einladung = {};
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
@@ -10,9 +11,29 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.render("index", {
-        ueberschrift: birthdayChild 
+        ueberschrift: birthdayChild,
+        ort: einladung.ort || "",
+        datum: einladung.datum || "",
+        uhrzeit: einladung.uhrzeit || "",
+        rückmeldung: einladung.rückmeldung
     });
 });
+
+app.post("/create", (req, res) => {
+    const {ort, datum, uhrzeit, rückmeldung} = req.body ;
+
+
+    einladung = {
+        ort,
+        datum,
+        uhrzeit, 
+        rückmeldung
+    };
+
+    console.log("🎉Neue Einladung gespeichert:", einladung);
+
+    res.redirect("/");
+})
 
 app.listen(port, () =>{
     console.log(`Server runing on ${port}`);
